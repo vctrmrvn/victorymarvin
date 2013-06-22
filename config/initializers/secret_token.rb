d23d9b051c77b6c73b54dc48eb2111dcb2130f7a
vctrmrvn <vctrmrvn@gmail.com>
2013-06-22 14:22:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Victorymarvin::Application.config.secret_token = '5ab0674cfac59279bfc2195b507916968c5f1b92a577de793781216cc30b02469fca1ea3f879f736c6fb47e5a4d6075d8a481b1dd38a1df0fda86731ecdfbef6'
