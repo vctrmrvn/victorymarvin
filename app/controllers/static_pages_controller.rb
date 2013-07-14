3dc9dc994ada895e1a687db152f13079b89d1219
vctrmrvn <vctrmrvn@gmail.com>
2013-07-14 17:35:27 -0400

    added monkeybusiness, lilac heading

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def monkeybusiness
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Victorymarvin::Application.routes.draw do
   resources :static_pages
 
   root to: 'static_pages#home'
+  match '/monkeybusiness' => 'static_pages#monkeybusiness', :as => :monkeybusiness
 end
